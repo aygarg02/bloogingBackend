c2e4263ab5acf2f95a520281a4c489f39e08b20d
aygarg02 <aygarg02@gmail.com>
2024-06-23 18:57:49 +0530

    domne

--- src/main/java/com/example/demo/model/Client.java
+++ src/main/java/com/example/demo/model/Client.java
@@ -18,7 +18,37 @@ import lombok.NoArgsConstructor;
 @Setter
 @Table(name="client")
 public class Client {
-    @Id
+    public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getAbout() {
+		return about;
+	}
+	public void setAbout(String about) {
+		this.about = about;
+	}
+	@Id
 private int id;
 private String  name;
 private String email;
--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,34 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
+import org.springframework.http.MediaType;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+@AutoConfiguration
 @SpringBootTest
 class DemoApplicationTests {
+@Autowired
+private MockMvc mockMvc;
+
 
 	@Test
 	void contextLoads() {
 	}
+	 void testGetClientById() throws Exception {
+        // Replace {id} with an actual ID for testing
+        int id = 1;
+System.out.println("this is worlnig fine");
+        mockMvc.perform(get("/api/users/")
+                .param("id", String.valueOf(id))
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(id)); // Replace with actual JSON property
+    }
+    
 
 }
